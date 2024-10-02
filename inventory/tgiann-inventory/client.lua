e7832c34404e9c443775388e9b6218242bcdaf2f
r_ <jayyc818@gmail.com>
2024-10-02 01:58:29 -0500

    refactor: finished tgiann-inventory compatibility

--- inventory/tgiann-inventory/client.lua
+++ inventory/tgiann-inventory/client.lua
@@ -8,7 +8,7 @@ function Core.Inventory.OpenStash(id)
     -- open stash export... not sure what it is yet. 
 end
 
-function Core.Inventory.GetItemInfo(item) -- NEEDS TESTING
+function Core.Inventory.GetItemInfo(item)
     local itemsList = exports['tgiann-inventory']:GetItemList()
     for _, itemData in pairs(itemsList) do
         if itemData.name == item then
--- inventory/tgiann-inventory/server.lua
+++ inventory/tgiann-inventory/server.lua
@@ -6,42 +6,46 @@ Core.Inventory = {}
 
 function Core.Inventory.AddItem(src, item, count, metadata)
     local src = src or source
-    return -- add item export
+    local action = exports['tgiann-inventory']:AddItem(src, item, count, metadata)
+    return (action.itemAddRemoveLog == 'added')
 end
 
 function Core.Inventory.RemoveItem(src, item, count, metadata)
     local src = src or source
-    return -- remove item export
-end
+    local action = exports['tgiann-inventory']:RemoveItem(src, item, count, nil, metadata)
+    return action
+end 
 
 function Core.Inventory.GetItem(src, item, metadata)
     local src = src or source
-    return exports['tgiann-inventory']:GetItemByName(src, item, metadata) -- NEEDS TESTED
+    local item = exports['tgiann-inventory']:GetItemByName(src, item, metadata)
+    item.count = item.amount
+    item.metadata = item.info
+    return item
 end
 
 function Core.Inventory.GetItemCount(src, item, metadata)
     local src = src or source
-    -- get items with export
-    -- return count if matches
-    return -- item count
+    local item = exports['tgiann-inventory']:GetItemByName(src, item, metadata)
+    return item.amount or 0
 end
 
 function Core.Inventory.GetInventoryItems(src)
     local src = src or source
-    return exports['tgiann-inventory']:GetPlayerItems(src) -- NEEDS TESTED
+    return exports['tgiann-inventory']:GetPlayerItems(src)
 end
 
 function Core.Inventory.CanCarryItem(src, item, count)
     local src = src or source
-    return exports["tgiann-inventory"]:CanCarryItem(src, item, count) -- NEEDS TESTED
+    return exports["tgiann-inventory"]:CanCarryItem(src, item, count)
 end
 
 function Core.Inventory.RegisterStash(id, label, slots, weight, owner)
-    -- have not found anything for this yet, good thing I dont use it
+    exports["tgiann-inventory"]:CreateCustomStashWithItem(id, {})
 end
 
 function Core.Inventory.GetItemInfo(item)
-    local itemsList = exports['tgiann-inventory']:GetItemList() -- NEEDS TESTED
+    local itemsList = exports['tgiann-inventory']:GetItemList()
     for _, itemData in pairs(itemsList) do
         if itemData.name == item then
             return itemData
@@ -51,5 +55,5 @@ end
 
 function Core.Inventory.SetMetadata(src, item, slot, metadata)
     local src = src or source
-    exports["tgiann-inventory"]:UpdateItemMetadata(src, item, slot, metadata) -- NEEDS TESTED
+    exports["tgiann-inventory"]:UpdateItemMetadata(src, item, slot, metadata)
 end
