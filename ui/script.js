a87362345dc80da99da9977c79df6a96769c3624
r_ <jayyc818@gmail.com>
2024-11-03 20:02:55 -0600

    refactor: changed text ui js logic

--- ui/script.js
+++ ui/script.js
@@ -1,20 +1,22 @@
 window.addEventListener('message', function (event) {
     if (event.data.type === "help-text") {
-        const helpText = document.querySelector('.help-text');
+        let helpText = document.querySelector('.help-text');
         if (event.data.display) {
             helpText.style.opacity = 1;
-            document.getElementById('help-text').textContent = event.data.text;
+            helpText.textContent = event.data.text;
         } else {
             helpText.style.opacity = 0;
         }
     }
 
     if (event.data.type === "text-ui") {
-        const textUI = document.querySelector('.text-ui');
+        let textUI = document.querySelector('.text-ui');
+        let key = document.querySelector('.text-ui button');
+        let text = document.querySelector('.text-ui p');
         if (event.data.display) {
+            key.textContent = event.data.key;
+            text.textContent = event.data.text;
             textUI.style.opacity = 1;
-            document.getElementById('text-ui-key').textContent = event.data.key;
-            document.getElementById('text-ui-text').textContent = event.data.text;
         } else {
             textUI.style.opacity = 0;
         }
