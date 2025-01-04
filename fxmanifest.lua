9907e55797479b2c2dd69fb8f82a574fd2af13ff
r_ <jayyc818@gmail.com>
2025-01-04 12:40:30 -0600

    chore: bump manifest version

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,7 +6,7 @@ lua54 'yes'
 name 'r_bridge'
 description 'Function library for r_scripts resources'
 author 'r_scripts'
-version '1.1.1'
+version '1.1.2'
 
 shared_scripts {
     '@ox_lib/init.lua',
