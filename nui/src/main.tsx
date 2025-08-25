21ca0186d71e73e9c239ed168241b101320ba50a
rumaier <148654629+rumaier@users.noreply.github.com>
2025-08-25 09:05:27 -0500

    fix: remove unused font styles import in main.tsx

--- nui/src/main.tsx
+++ nui/src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import '@mantine/core/styles.css';
 import './index.css'
-import './fonts.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
