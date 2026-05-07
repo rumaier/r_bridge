1c338ba61756497bb8b36227a35d66bb62c44923
rumaier <148654629+rumaier@users.noreply.github.com>
2026-05-06 19:50:42 -0500

    chore: refactor

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -7,37 +7,20 @@ lua54 'yes'
 name 'r_bridge'
 description 'Compatibility layer for r_scripts resources'
 author 'rumaier'
-version '2.0.8'
+version '3.0.0'
 
 shared_scripts {
-  '@ox_lib/init.lua',
-  'util/init.lua',
-  'util/natives.lua',
-  'util/inventory.lua',
+
 }
 
 server_scripts {
   'framework/**/server.lua',
-  'inventory/**/server.lua',
-  'target/**/server.lua',
-  'util/version.lua',
 }
 
 client_scripts {
-  'util/natives.lua',
   'framework/**/client.lua',
-  'inventory/**/client.lua',
-  'target/**/client.lua',
-  'interface/*.lua',
-}
-
-ui_page 'nui/build/index.html'
-
-files {
-  'nui/build/index.html',
-  'nui/build/**/*'
 }
 
 dependencies {
-  'ox_lib',
+  -- 'ox_lib',
 }
