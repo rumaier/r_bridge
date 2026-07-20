35ebe33086ca874cf9153bbd50fddefb3eb4122d
rumaier <148654629+rumaier@users.noreply.github.com>
2026-07-19 19:51:22 -0500

    fix: call on correct entity in object placer

--- imports/utility/client.lua
+++ imports/utility/client.lua
@@ -17,36 +17,36 @@ Utility.useObjectPlacer = function(model, offset, rotation, minDistance, snapToG
     if placerState.active then return end
     local inbounds = true
     local maxDistance = (minDistance or 1.0) + 5.0
-    local heading = (GetGameplayCamRot(0).z + 360) % 360 - rotation.z
     offset = offset or vec3(0, 0, 0)
     rotation = rotation or vec3(0, 0, 0)
+    local heading = (GetGameplayCamRot(0).z + 360) % 360 - rotation.z
     placerState.active = true
     placerState.entity = bridge.natives.createObject(model, vec3(0, 0, 0), heading, false)
     SetEntityAlpha(placerState.entity, 150, false)
     SetEntityDrawOutlineColor(255, 0, 0, 255)
-    SetEntityCompletelyDisableCollision(entity, false, true)
-    SetEntityInvincible(entity, true)
+    SetEntityCompletelyDisableCollision(placerState.entity, false, true)
+    SetEntityInvincible(placerState.entity, true)
     bridge.interface.showTextUi('[E] Place  \n[X] Cancel  \n[Scroll Up] Turn Left  \n[Scroll Down] Turn Right')
     while placerState.active and DoesEntityExist(placerState.entity) do
         local hit, _, coords, _, terrain = lib.raycast.fromCamera(1, 4)
         if hit then
-            SetEntityCoords(entity, coords.x, coords.y, coords.z, false, false, false, false)
-            SetEntityRotation(entity, rotation.x, rotation.y, heading, 2, true)
+            SetEntityCoords(placerState.entity, coords.x, coords.y, coords.z, false, false, false, false)
+            SetEntityRotation(placerState.entity, rotation.x, rotation.y, heading, 2, true)
             local playerCoords = GetEntityCoords(cache.ped)
             local distance = #(playerCoords - coords)
 
             if snapToGround then
-                PlaceObjectOnGroundOrObjectProperly(entity)
+                PlaceObjectOnGroundOrObjectProperly(placerState.entity)
             end
 
             if not inbounds then
                 inbounds = true
-                SetEntityDrawOutline(entity, false)
+                SetEntityDrawOutline(placerState.entity, false)
             end
 
             if distance > maxDistance and distance < minDistance or (allowedTerrain and not allowedTerrain[terrain]) and inbounds then
                 inbounds = false
-                SetEntityDrawOutline(entity, true)
+                SetEntityDrawOutline(placerState.entity, true)
             end
 
             if IsControlJustReleased(0, 14) then
@@ -58,8 +58,12 @@ Utility.useObjectPlacer = function(model, offset, rotation, minDistance, snapToG
             end
 
             if IsControlJustPressed(0, 73) or IsControlJustReleased(0, 73) then
-                cleanup()
-                break
+                SetEntityDrawOutlineColor(255, 0, 0, 255)
+                DeleteEntity(placerState.entity)
+                bridge.interface.hideTextUi()
+                placerState.active = false
+                placerState.entity = nil
+                return nil
             end
 
             if IsControlJustPressed(0, 38) or IsControlJustReleased(0, 38) and inbounds then
